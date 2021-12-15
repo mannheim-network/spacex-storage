fe517f98f3da6f44bdb594735dd48ad7215a008d
aexkobe <calito9080@gmail.com>
2021-12-15 14:10:23 +0800

    add dabaase and http

---
+++ src/app/chain/Chain.cpp
@@ -0,0 +1,439 @@
+#include "Chain.h"
+
+spacex::Log *p_log = spacex::Log::get_instance();
+HttpClient *pri_chain_client = NULL;
+extern bool offline_chain_mode;
+
+namespace spacex
+{
+
+Chain *Chain::chain = NULL;
+std::mutex chain_mutex;
+
+/**
+ * @description: single instance class function to get instance
+ * @return: chain instance
+ */
+Chain *Chain::get_instance()
+{
+    if (Chain::chain == NULL)
+    {
+        Config *p_config = Config::get_instance();
+        chain_mutex.lock();
+        if (Chain::chain == NULL)
+        {
+            Chain::chain = new Chain(p_config->chain_api_base_url, p_config->chain_password, p_config->chain_backup, offline_chain_mode);
+        }
+        chain_mutex.unlock();
+    }
+
+    return Chain::chain;
+}
+
+/**
+ * @description: new a chain handler to access chain node
+ * @param url -> chain API base url, like: http://127.0.0.1:56666/api/v1
+ * @param password_tmp -> the password of chain account id
+ * @param backup_tmp ->  the backup of chain account id
+ * @param is_offline -> Off chain mode or not
+ */
+Chain::Chain(std::string url, std::string password_tmp, std::string backup_tmp, bool is_offline)
+{
+    this->url = url;
+    this->password = password_tmp;
+    this->backup = backup_tmp;
+    this->is_offline = is_offline;
+    this->offline_block_height = 0;
+    pri_chain_client = new HttpClient();
+}
+
+/**
+ * @description: destructor
+ */
+Chain::~Chain()
+{
+    if (pri_chain_client != NULL)
+    {
+        delete pri_chain_client;
+        pri_chain_client = NULL;
+    }
+}
+
+/**
+ * @description: get laster block header from chain
+ * @param block_header -> Reference to block_header
+ * @return: the point of block header
+ */
+bool Chain::get_block_header(BlockHeader &block_header)
+{
+    if (this->is_offline)
+    {
+        offline_block_height_mutex.lock();
+        if (this->offline_block_height == 0)
+        {
+            this->offline_block_height = this->get_offline_block_height();
+        }
+        block_header.number = this->offline_block_height;
+        offline_block_height_mutex.unlock();
+
+        block_header.hash = "1000000000000000000000000000000000000000000000000000000000000001";
+        return true;
+    }
+
+    std::string path = this->url + "/block/header";
+    http::response<http::string_body> res = pri_chain_client->Get(path.c_str());
+    if ((int)res.result() == 200)
+    {
+        json::JSON block_header_json = json::JSON::Load_unsafe(res.body());
+        block_header.hash = block_header_json["hash"].ToString().erase(0,2);
+        block_header.number = block_header_json["number"].ToInt();
+        return true;
+    }
+
+    if (res.body().size() != 0)
+    {
+        p_log->err("%s\n", res.body().c_str());
+    }
+    else
+    {
+        p_log->err("%s\n", "Get block header:return body is null");
+    }
+
+    return false;
+}
+
+
+/**
+ * @description: get block hash by number
+ * @param block_number block number
+ * @return: block hash
+ */
+std::string Chain::get_block_hash(size_t block_number)
+{
+    if (this->is_offline)
+    {
+        return "1000000000000000000000000000000000000000000000000000000000000001";
+    }
+
+    std::string url = this->url + "/block/hash?blockNumber=" + std::to_string(block_number);
+    http::response<http::string_body> res = pri_chain_client->Get(url.c_str());
+    std::string res_body = res.body();
+    if ((int)res.result() == 200)
+    {
+        if (res_body.size() > 3)
+        {
+            return res_body.substr(3, 64);
+        }
+    }
+
+    if (res_body.size() != 0)
+    {
+        p_log->info("%s\n", res_body.c_str());
+    }
+    else
+    {
+        p_log->err("%s\n", "Get block hash:return body is null");
+    }
+
+    return "";
+}
+
+
+/**
+ * @description: get block hash by number
+ * @return: swork code on chain
+ */
+std::string Chain::get_swork_code()
+{
+    if (this->is_offline)
+    {
+        return "04579f4102301d39f68032446b63fc0cede4817cf099312a0c397a760651af98";
+    }
+
+    std::string url = this->url + "/swork/code";
+    http::response<http::string_body> res = pri_chain_client->Get(url.c_str());
+    std::string res_body = res.body();
+    if ((int)res.result() == 200)
+    {
+        if (res_body.size() > 3)
+        {
+            return res_body.substr(3, 64);
+        }
+    }
+
+    if (res_body.size() != 0)
+    {
+        p_log->info("%s\n", res_body.c_str());
+    }
+    else
+    {
+        p_log->err("%s\n", "Get swork code:return body is null");
+    }
+
+    return "";
+}
+
+/**
+ * @description: test if chian is online
+ * @return: test result
+ */
+bool Chain::is_online(void)
+{
+    if (this->is_offline)
+    {
+        return true;
+    }
+
+    std::string path = this->url + "/block/header";
+    http::response<http::string_body> res = pri_chain_client->Get(path.c_str());
+    if ((int)res.result() == 200)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+/**
+ * @description: test if chian is syncing
+ * @return: test result
+ */
+bool Chain::is_syncing(void)
+{
+    if (this->is_offline)
+    {
+        return false;
+    }
+
+    std::string path = this->url + "/system/health";
+    http::response<http::string_body> res = pri_chain_client->Get(path.c_str());
+    if ((int)res.result() == 200)
+    {
+        json::JSON system_health_json = json::JSON::Load_unsafe(res.body());
+        return system_health_json["isSyncing"].ToBool();
+    }
+
+    if (res.body().size() != 0)
+    {
+        p_log->err("%s\n", res.body().c_str());
+    }
+    else
+    {
+        p_log->err("%s\n", "Is syncing:return body is null");
+    }
+
+    return true;
+}
+
+/**
+ * @description: waiting for the spacex chain to run
+ * @return: success or not
+ */
+bool Chain::wait_for_running(void)
+{
+    if (this->is_offline)
+    {
+        return true;
+    }
+
+    size_t start_block_height = 10;
+
+    while (true)
+    {
+        if (this->is_online())
+        {
+            break;
+        }
+        else
+        {
+            p_log->info("Waiting for chain to run...\n");
+            sleep(3);
+        }
+    }
+
+    while (true)
+    {
+        spacex::BlockHeader block_header;
+        if (!this->get_block_header(block_header))
+        {
+            p_log->info("Waiting for chain to run...\n");
+            sleep(3);
+            continue;
+        }
+
+        if (block_header.number >= start_block_height)
+        {
+            break;
+        }
+        else
+        {
+            p_log->info("Wait for the chain to execute after %lu blocks, now is %lu ...\n", start_block_height, block_header.number);
+            sleep(3);
+        }
+    }
+
+    while (true)
+    {
+        if (!this->is_syncing() && !this->is_syncing())
+        {
+            break;
+        }
+        else
+        {
+            spacex::BlockHeader block_header;
+            if (this->get_block_header(block_header))
+            {
+                p_log->info("Wait for chain synchronization to complete, currently synchronized to the %lu block\n", block_header.number);
+            }
+            else
+            {
+                p_log->info("Waiting for chain to run...\n");
+            }
+            sleep(6);
+        }   
+    }
+
+    return true;
+}
+
+/**
+ * @description: post sworker identity to chain chain
+ * @param identity -> sworker identity
+ * @return: success or fail
+ */
+bool Chain::post_sworker_identity(std::string identity)
+{
+    if (this->is_offline)
+    {
+        return true;
+    }
+
+    for (int i = 0; i < 20; i++)
+    {
+        std::string path = this->url + "/swork/identity";
+        ApiHeaders headers = {{"password", this->password}, {"Content-Type", "application/json"}};
+
+        spacex_status_t spacex_status = SPACEX_SUCCESS;
+        json::JSON obj = json::JSON::Load(&spacex_status, identity);
+        if (SPACEX_SUCCESS != spacex_status)
+        {
+            p_log->err("Parse sworker identity failed! Error code:%lx\n", spacex_status);
+            return false;
+        }
+        obj["backup"] = this->backup;
+        http::response<http::string_body> res = pri_chain_client->Post(path.c_str(), obj.dump(), "application/json", headers);
+
+        if ((int)res.result() == 200)
+        {
+            return true;
+        }
+
+        if (res.body().size() != 0)
+        {
+            p_log->err("Chain result: %s, wait 10s and try again\n", res.body().c_str());
+        }
+        else
+        {
+            p_log->err("Chain result: %s, wait 10s and try again\n", "upload identity:return body is null");
+        }
+
+        sleep(10);
+    }
+
+    return false;
+}
+
+/**
+ * @description: post sworker work report to chain
+ * @param work_report -> sworker work report
+ * @return: success or fail
+ */
+bool Chain::post_sworker_work_report(std::string work_report)
+{
+    if (this->is_offline)
+    {
+        return true;
+    }
+
+    for (int i = 0; i < 20; i++)
+    {
+        std::string path = this->url + "/swork/workreport";
+        ApiHeaders headers = {{"password", this->password}, {"Content-Type", "application/json"}};
+
+        spacex_status_t spacex_status = SPACEX_SUCCESS;
+        json::JSON obj = json::JSON::Load(&spacex_status, work_report);
+        if (SPACEX_SUCCESS != spacex_status)
+        {
+            p_log->err("Parse sworker workreport failed! Error code:%lx\n", spacex_status);
+            return false;
+        }
+        obj["backup"] = this->backup;
+        http::response<http::string_body> res = pri_chain_client->Post(path.c_str(), obj.dump(), "application/json", headers);
+
+        if ((int)res.result() == 200)
+        {
+            return true;
+        }
+
+        if (res.body().size() != 0)
+        {
+            json::JSON res_json = json::JSON::Load_unsafe(res.body());
+            std::string msg = res_json["message"].ToString();
+            if (msg == "swork.InvalidReportTime")
+            {
+                p_log->err("Chain error: %s. Please check the synchronization of the chain!\n", res.body().c_str());
+                return false;
+            } 
+            else if (msg == "swork.IllegalReporter")
+            {
+                p_log->err("Chain error: %s. The current account does not match the original account, please stop sworker and reconfigure! SF:WRE\n", res.body().c_str());
+                return false;
+            }
+            else if (msg == "swork.OutdatedReporter")
+            {
+                p_log->err("Chain error: %s. The current sworker has expired, please shovel the data and run a new sworker! SF:WRE\n", res.body().c_str());
+                return false;
+            }
+            else if (msg == "swork.IllegalWorkReportSig" || msg == "swork.IllegalFilesTransition" || msg == "swork.ABUpgradeFailed")
+            {
+                p_log->err("Chain error: %s SF:WRE\n", res.body().c_str());
+                return false;
+            }
+            else
+            {
+                p_log->err("Chain error: %s, wait 10s and try again. SF:WRE\n", res.body().c_str());
+            }
+        }
+        else
+        {
+            p_log->err("Chain result: %s, wait 10s and try again. SF:WRE\n", "upload workreport:return body is null");
+        }
+        
+        sleep(10);
+    }
+
+    return false;
+}
+
+size_t Chain::get_offline_block_height(void)
+{
+    std::string offline_base_height_str = "";
+    if (spacex::DataBase::get_instance()->get("offline_block_height_key", offline_base_height_str) == SPACEX_SUCCESS)
+    {
+        std::stringstream sstream(offline_base_height_str);
+        size_t h = 0;
+        sstream >> h;
+        return h;
+    }
+    return 0;
+}
+
+void Chain::add_offline_block_height(size_t h)
+{
+    offline_block_height_mutex.lock();
+    spacex::DataBase::get_instance()->set("offline_block_height_key", std::to_string(this->offline_block_height + h));
+    this->offline_block_height += h;
+    offline_block_height_mutex.unlock();
+}
+
+} // namespace spacex
---
+++ src/app/http/ApiHandler.cpp
@@ -0,0 +1,100 @@
+#include "ApiHandler.h"
+
+namespace beast = boost::beast;         // from <boost/beast.hpp>
+namespace http = beast::http;           // from <boost/beast/http.hpp>
+namespace websocket = beast::websocket; // from <boost/beast/websocket.hpp>
+namespace net = boost::asio;            // from <boost/asio.hpp>
+namespace ssl = boost::asio::ssl;       // from <boost/asio/ssl.hpp>
+using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
+
+
+extern sgx_enclave_id_t global_eid;
+
+spacex::Log *p_log = spacex::Log::get_instance();
+
+/**
+ * @description: Append an HTTP rel-path to a local filesystem path.
+ *  The returned path is normalized for the platform.
+ * @param base -> Url base path
+ * @param path -> Url specific path
+ * @return: Analyzed path
+ */
+std::string path_cat(beast::string_view base, beast::string_view path)
+{
+    if(base.empty())
+        return std::string(path);
+    std::string result(base);
+#ifdef BOOST_MSVC
+    char constexpr path_separator = '\\';
+    if(result.back() == path_separator)
+        result.resize(result.size() - 1);
+    result.append(path.data(), path.size());
+    for (auto& c : result)
+        if(c == '/')
+            c = path_separator;
+#else
+    char constexpr path_separator = '/';
+    if(result.back() == path_separator)
+        result.resize(result.size() - 1);
+    result.append(path.data(), path.size());
+#endif
+    return result;
+}
+
+/**
+ * @description: Get url parameters
+ * @param url -> Request URL
+ * @return: Key value pair url parameters
+ */
+std::map<std::string, std::string> get_params(std::string &url)
+{
+    std::map<std::string, std::string> ans;
+    size_t spos = url.find('\?');
+    size_t epos;
+    if (spos == std::string::npos)
+    {
+        ans["req_route"] = url;
+        return ans;
+    }
+    ans["req_route"] = url.substr(0, spos);
+    spos++;
+    while (spos < url.size())
+    {
+        epos = url.find('&', spos);
+        if (epos == std::string::npos)
+        {
+            epos = url.size();
+        }
+        size_t ppos = url.find('=', spos);
+        if (ppos > epos || ppos == std::string::npos)
+        {
+            return ans;
+        }
+        std::string key = url.substr(spos, ppos - spos);
+        ppos++;
+        std::string val = url.substr(ppos, epos - ppos);
+        ans[key] = val;
+
+        spos = epos + 1;
+    }
+
+    return ans;
+}
+
+/**
+ * @description: Handle websocket request
+ * @param path -> Request path
+ * @param data -> Request data
+ * @param close_connection -> Indicate whether to close connection
+ * @return: Response data as json format
+ */
+std::string ApiHandler::websocket_handler(std::string &/*path*/, std::string &/*data*/, bool &/*close_connection*/)
+{
+    //Config *p_config = Config::get_instance();
+    json::JSON res;
+    //UrlEndPoint url_end_point = get_url_end_point(p_config->base_url);
+    res["status"] = 300;
+    res["body"] = "Websocket doesn't provide service now!";
+
+    return res.dump();
+}
